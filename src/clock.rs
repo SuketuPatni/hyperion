c2c9e052bce734f2fcd9603129f76df13cf887f7
SuketuPatni <suketublackpanther9@gmail.com>
2021-11-25 22:05:38 +0530

    beautiful clock render added

---
+++ src/clock.rs
@@ -0,0 +1,50 @@
+use std::fs;
+
+#[path = "canvas.rs"]
+mod canvas;
+
+#[path = "matrix.rs"]
+mod matrix;
+
+pub fn clock_render() {
+    let mut canvas1 = canvas::canvas(40,40);
+    // origin is (20,20)
+    let yellow = canvas::color(1.0,1.0,0.0);
+    
+    // length of clock arm is 15 pixels
+    let mut p_12 = matrix::tuples::point(0.0,15.0,0.0);
+
+    let rotate = matrix::rotation_z(3.14159265/6.0);
+
+    for _ in 0..12 {
+        // (0,15) should become (20,5)
+        // So always (x + 20, 20 - y)
+        let temp = matrix::multiply_matrix_tup(rotate,p_12);
+        println!("{:?}", temp);
+        p_12 = temp;
+        canvas1 = canvas::write_pixel(
+            canvas1, (temp.x.ceil() + 20.0) as usize, (20.0 - temp.y.ceil()) as usize, yellow
+        );
+        // points_vec.push(matrix::multiply_matrix_tup(rotate,p_12));
+    }
+
+    let mut ppm_string = canvas::header(40,40);
+
+    for i in canvas1 {
+        if ppm_string.len() % 70 == 69 {
+            ppm_string.push_str("\n");
+        }
+        for j in i {
+            ppm_string.push_str(&((j.red * 255.0).ceil()).to_string());
+            ppm_string.push_str(" ");
+            ppm_string.push_str(&((j.green * 255.0).ceil()).to_string());
+            ppm_string.push_str(" ");
+            ppm_string.push_str(&((j.blue * 255.0).ceil()).to_string());
+            ppm_string.push_str(" ");
+        }
+        ppm_string.push_str("\n");
+    }
+    
+    fs::write("renders/clock.ppm", ppm_string).expect("");  
+
+}
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ mod tuples;
 mod canvas;
 mod projectile;
 mod matrix;
+mod clock;
 
 fn main() {
     // tuples::tuple_tests();
     // canvas::canvas_tests();
     // projectile::projectile_plot();
     // matrix::matrix_tests();
-    matrix::transform_tests();
+    // matrix::transform_tests();
+    clock::clock_render();
 }
